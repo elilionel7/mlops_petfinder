17c4036ffc0b70c306f71fd70c50a895b0e75176
elilionel7 <elilionel7@gmail.com>
2023-10-29 14:37:07 -0500

    cols_cofig contains type of encoding needed on which column

---
+++ src/data_scripts/column_cofig.py
@@ -0,0 +1,16 @@
+"""
+dict
+key: what sort of encoding needed to be performed
+values: columns that need to be encoded base of their key(encoding)
+"""
+cols_config = {
+    "one_hot_encode_cols": ["Type", "Gender"],
+    "label_encode_cols": ["Vaccinated", "Sterilized", "Color1", "Color2"],
+    "ordinal_encode_cols": {
+        "Health": ["Healthy", "Minor Injury", "Serious Injury"],
+        "FurLength": ["Short", "Medium", "Long"],
+        "MaturitySize": ["Small", "Medium", "Large"],
+    },
+    "count_encode_col": "Breed1",
+    "target_col": "Adopted",
+}
